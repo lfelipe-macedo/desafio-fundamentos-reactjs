fec859846ad15349603e16bf0a66a6505c932aeb
Felipe Macedo <felipebmacedo.91@gmail.com>
2020-05-02 05:14:01 -0300

    Voltando jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,3 @@
-const {resolve} = require( 'path');
 module.exports = {
-  "roots": [
-    `${resolve(__dirname)}`
-  ],
-  "testMatch": [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  },
-}
+  preset: 'ts-jest',
+};
